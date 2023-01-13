238a7e0d8ce96954dd912d2bdd8964630dad82e1
Mavik <mavik01@yandex.ru>
2023-01-13 22:10:13 +0300

    dd solution for tassk 0,1,3,5,7

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №0
+// Напишите программу, которая на вход принимает
+// число и выдает его квадрат (число умноженное на
+// само себя)
+
+String? inputNum = Console.ReadLine();
+if(inputNum!=null)
+
+{
+
+    int number = int.Parse(inputNum);
+
+    int result = number*number;
+
+
+    Console.WriteLine(result);
+
+
+}
+
