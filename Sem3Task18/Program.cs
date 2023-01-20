b1789424289e120468d165153371e8fed14a2cbf
Mavik <mavik01@yandex.ru>
2023-01-20 22:01:50 +0300

    add solution 17,18,20,22

---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача №18
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int qNum = ReadData("Введите координату х");
+
+PrintQuterAnswer(qNum);
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+// Метод определяет четверть по координатам точки
+void PrintQuterAnswer(int num)
+{
+if (num > 0 && num < 5)
+{
+    if (num == 1) Console.WriteLine("X > 0, Y > 0");
+    if (num == 2) Console.WriteLine("X > 0, Y < 0");
+    if (num == 3) Console.WriteLine("X < 0, Y < 0");
+    if (num == 4) Console.WriteLine("X < 0, Y > 0");
+}
+else
+{
+    Console.WriteLine("Это не номер четверти");
+}
+}
