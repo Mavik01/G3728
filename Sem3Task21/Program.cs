8689bc182678ef5e3648c79dd4449ef4fb30614f
Mavik <mavik01@yandex.ru>
2023-01-23 22:58:46 +0300

    add solution 19,21,23

--- Sem3Task21/Program.cs
+++ Sem3Task21/Program.cs
@@ -1,9 +1,6 @@
-﻿// Задача №20
-// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
-// Например:
-// A (3,6); B (2,1) -> 5,09
-// A (7,-5); B (1,-1) -> 7,21
-
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 // Метод читает данные от пользователя
 int ReadData(string msg)
 {
@@ -15,11 +12,11 @@ void PrintData(string msg, double res)
 {
     Console.WriteLine(msg + res);
 }
-// Вычисляем расстояние между точками в 2D пространстве
-double CalcLen(int x1, int x2, int y1, int y2)
+// Вычисляем расстояние между точками в 3D пространстве
+double CalcLen(int x1, int x2, int y1, int y2, int z1, int z2)
 {
     double res = 0;
-    res = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2));
+    res = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
     return res;
 }
 // Воодим координаты точек
@@ -27,8 +24,11 @@ int coordX1 = ReadData("Введите координату X1");
 int coordX2 = ReadData("Введите координату X2");
 int coordY1 = ReadData("Введите координату Y1");
 int coordY2 = ReadData("Введите координату Y2");
+int coordZ1 = ReadData("Введите координату Z1");
+int coordZ2 = ReadData("Введите координату Z2");
+
 // Вычисляем длину
-double len = CalcLen(coordX1,coordX2,coordY1,coordY2);
+double len = CalcLen(coordX1,coordX2,coordY1,coordY2,coordZ1,coordZ2);
 
 // Выдаем результат
 PrintData("Расстояние между точками: ",len);
