c061138ad09785c2d203ff8a06fb4bee51caad5d
Mavik <mavik01@yandex.ru>
2023-01-24 22:15:52 +0300

    add solution 24,26,28,30

--- Sem3Task19/Program.cs
+++ Sem3Task19/Program.cs
@@ -2,24 +2,88 @@
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
 // Считываем данные пользователя
-Console.WriteLine("Введите число: ");
-string pal = Console.ReadLine();
+// Console.WriteLine("Введите число: ");
+// string pal = Console.ReadLine();
 
-// Вводим переменную равную длине массива
-int n = pal.Length;
+// // Вводим переменную равную длине массива
+// int n = pal.Length;
 
-if (n == 5)
+// if (n == 5)
+// {
+//     if (pal[0] == pal[4] && pal[1] == pal[3])
+//     {
+//         Console.WriteLine("Палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine("не палиндром");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("не пятизначное число");
+// }
+
+// Метод считывания данных пользователя
+string ReadData(string line)
 {
-    if (pal[0] == pal[4] && pal[1] == pal[3])
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем строку
+    string inputLine = Console.ReadLine() ?? "";
+    // Возвращаем значение
+    return inputLine;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+// Решение "со звёздочкой" 2. Определение палиндрома для пятизначного числа с помощью словаря
+// Метод, возвращающий словарь четырехзначных палиндромов
+Dictionary<int, int> FillPalindroms()
+{
+    Dictionary<int, int> palindroms = new Dictionary<int, int>();
+
+    // Цикл внутри цикла, заполняющих значения словаря палиндромов
+    for (int i = 1; i < 10; i++)
     {
-        Console.WriteLine("Палиндром");
+        for (int j = 0; j < 10; j++)
+        {
+            int key = j * 10 + i;
+            int value = i * 1000 + j * 100 + j * 10 + i;
+            palindroms.Add(key, value);
+        }
+    }
+
+    return palindroms;
+}
+// Метод, проверяющий на палиндром
+bool CheckIfPalindrom(int inputNumber, Dictionary<int, int> palindroms)
+{
+    // Делаем из исходного пятизначного числа четырёхзначное, убирая третью цифру
+    int fourDigitNumber = (inputNumber / 1000) * 100 + inputNumber % 100;
+    // Проверка на вхождение в словарь палиндромов
+    return palindroms.ContainsValue(fourDigitNumber);
+}
+
+int inputNumber = 0;
+string inputLine = ReadData("Введите пятизначное число: ");
+if (int.TryParse(inputLine, out inputNumber))
+{
+    if (inputNumber > 9999 && inputNumber < 100000)
+    {
+        Dictionary<int, int> fourDigitPalindroms = FillPalindroms();
+        PrintResult(CheckIfPalindrom(inputNumber, fourDigitPalindroms) ? "Это палиндром" : "Это не палиндром");
     }
     else
     {
-        Console.WriteLine("не палиндром");
+        PrintResult("Это не пятизначное число");
     }
 }
 else
 {
-    Console.WriteLine("не пятизначное число");
+    PrintResult("Это не число");
 }
