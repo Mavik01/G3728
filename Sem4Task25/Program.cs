0e8316a4dd7ec89bdb9bb0a84e24c9ab61a93326
Mavik <mavik01@yandex.ru>
2023-01-26 22:46:38 +0300

    add solution 25,27,29

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+//Выводим результат пользователю
+void PrintData(string msg1, int msg2)
+{
+    Console.Write(msg1);
+    Console.WriteLine(msg2);
+}
+// Пишем метод возведения в степень числа А
+int Pow (int A, int B)
+
+{
+    int res = 1;
+    while (B>0)
+    {
+        B=B-1;
+        res = res*A;        
+    }
+    return res;
+}
+
+int numA = ReadData("Введите число A: ");
+int numB = ReadData("Введите степень: ");
+
+// Обратились к методу который возводит в степень В число А
+int res = Pow (numA, numB);
+
+// Выводим результат на консоль
+PrintData($"Число {numA} в степени {numB} равно ",res);
+
---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+//Выводим результат пользователю
+void PrintData(string msg1, int msg2)
+{
+    Console.Write(msg1);
+    Console.WriteLine(msg2);
+}
+
+int SumSym (int num)
+
+{
+    int res = 0;
+    while (num>0)
+    {
+        res = res + num%10;
+        num = num/10;
+    }
+    return res;
+}
+
+int num = ReadData("Введите число: ");
+
+// Обратились к методу который суммирует цифры
+int res = SumSym(num);
+
+// Выводим результат на консоль
+PrintData($"Сумма цифр числа {num} равна ",res);
